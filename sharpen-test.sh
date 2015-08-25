d01a783a7b06ed14ee6392772283d7733ba295c7
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2015-08-25 15:01:55 -0700

    initial veriosn of script with repos ti be used

---
+++ sharpen-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+git clone --recursive \
+	https://github.com/mono/sharpen.git
+
+git clone --recursive \
+	https://github.com/ydanila/sharpen_imazen_config.git
+
+git clone --recursive \
+	https://github.com/ydanila/n-metadata-extractor.git
+
+git clone --recursive \
+	https://github.com/layerhq/quick-start-android.git

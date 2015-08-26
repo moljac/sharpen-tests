2a16a1327e4d098a8992dc13dbbf563217c62978
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2015-08-26 04:41:21 -0700

    sharpen-test script update - config does not work yet

--- sharpen-test.sh
+++ sharpen-test.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
 
-
+rm -fr sharpen.mono/
 git clone --recursive \
-	https://github.com/mono/sharpen.git
+	https://github.com/mono/sharpen.git \
+	sharpen.mono/
 
+rm -fr sharpen.sample.config/
 git clone --recursive \
-	https://github.com/ydanila/sharpen_imazen_config.git
+	https://github.com/ydanila/sharpen_imazen_config.git \
+	sharpen.sample.config/
 
+rm -fr sharpen.sample.java.repo.for.convresion/
 git clone --recursive \
-	https://github.com/ydanila/n-metadata-extractor.git
+	-b xmp-core \
+	https://github.com/ydanila/n-metadata-extractor.git \
+	sharpen.sample.java.repo.for.convresion/
 
+rm -fr sharpen.sample.java.android.repo.for.convresion/
 git clone --recursive \
-	https://github.com/layerhq/quick-start-android.git
+	https://github.com/layerhq/quick-start-android.git \
+	sharpen.sample.java.android.repo.for.convresion/
+
+
+#------------------------------------------------------------------------------
+# maven fixes
+
+cp -f \
+	sharpen.fixes/pom.mono.sharpen.fix.xml \
+	sharpen.mono/pom.xml
+cd sharpen.mono/
+mvn clean test
+mvn clean package
+ls -al ./src/target/*.ja
+cd ..
+
+cp -f \
+	sharpen.fixes/pom.config.project.fix.xml \
+	sharpen.sample.config/pom.xml
+cd sharpen.sample.config/
+mvn clean package
+ls -al ./sharpen.config/target/*.jar
+
+
+cd sharpen.sample.java.repo.for.convresion/
+java \
+	-jar ../sharpen.mono/src/target/sharpencore-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+	src/ 
+
+java \
+	-jar ../sharpen.mono/src/target/sharpencore-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+	src/ \
+	-configurationClass \
+		../sharpen.sample.config/sharpen.config/target/MEConfiguration.sharpenconfig.jar \
+	@sharpen-all-options
+
+\
+	@sharpen-all-options-without-configuration
